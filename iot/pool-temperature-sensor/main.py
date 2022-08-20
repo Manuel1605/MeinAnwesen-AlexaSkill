79c4983cc8fc64e59035747502306cc1ce38156a
Manuel <manuelfasching@gmail.com>
2022-08-20 17:28:42 +0200

    Execute program in infinite loop. Use Library to read sensor data.

--- iot/pool-temperature-sensor/main.py
+++ iot/pool-temperature-sensor/main.py
@@ -5,9 +5,9 @@ import sensor
 
 ENDPOINT = "ao9qxmwglcuca-ats.iot.eu-central-1.amazonaws.com"
 CLIENT_ID = "54dddebc-a706-4439-8fc4-bb8b41a6b4e4"
-PATH_TO_CERT = "certificates/34686ff72b-certificate.pem.crt"
-PATH_TO_KEY = "certificates/34686ff72b-private.pem.key"
-PATH_TO_ROOT = "certificates/root.pem"
+PATH_TO_CERT = "/data/certificates/34686ff72b-certificate.pem.crt"
+PATH_TO_KEY = "/data/certificates/34686ff72b-private.pem.key"
+PATH_TO_ROOT = "/data/certificates/root.pem"
 TOPIC = "pool-temperature"
 
 myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(CLIENT_ID)
@@ -16,7 +16,9 @@ myAWSIoTMQTTClient.configureCredentials(PATH_TO_ROOT, PATH_TO_KEY, PATH_TO_CERT)
 
 myAWSIoTMQTTClient.connect()
 
-temp = sensor.read_temp()
-message = {"sensorId": "pool_temp_1", "sensor_value": temp, "timestamp": round(t.time() * 1000)}
-myAWSIoTMQTTClient.publish(TOPIC, json.dumps(message), 1)
+while(True):
+    temp = sensor.read_temp()
+    message = {"sensorId": "pool_temp_1", "sensor_value": temp, "timestamp": round(t.time() * 1000)}
+    myAWSIoTMQTTClient.publish(TOPIC, json.dumps(message), 1)
+    t.sleep(10*60)
 myAWSIoTMQTTClient.disconnect()
